b8c61d5fd3ae0f9dec6279f3cb43e60d0d72a149
J <mosinabrejp@students.nu-dasma.edu.ph>
2024-12-24 19:38:04 +0800

    add blur effect

--- hide-show-bag.js
+++ hide-show-bag.js
@@ -4,9 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const showBag = document.querySelector('#show-bag-btn');
     const hideBag = document.querySelector('#hide-bag-btn');
 
+    const bodyBlur = document.querySelector('#target-body');
+
     showBag.addEventListener('click', (e) => {
         bagContainer.style.animationName = 'show';
         bagContainer.classList.add('show');
+
+        bodyBlur.style.webkitFilter = 'blur(5px)'
     });
 
 
@@ -15,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
         bagContainer.style.animationName = 'hide';
         setTimeout(() => {
             bagContainer.classList.remove('show');
+            bodyBlur.style.webkitFilter = 'blur(0)'
         }, 1000);
         
     });
