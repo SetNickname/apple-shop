13d166f14a98cb7f1b453fb23bd70f71ca358391
J <mosinabrejp@students.nu-dasma.edu.ph>
2025-03-02 12:44:09 +0800

    add new tables for order and products

--- queries.sql
+++ queries.sql
@@ -12,3 +12,21 @@ CREATE TABLE User (
 	password varchar(40),
 	primary key(user_id)
 );
+
+CREATE TABLE Products (
+	product_id int NOT NULL AUTO_INCREMENT,
+    product_name varchar(255) NOT NULL,
+    primary key (product_id)
+);
+
+CREATE TABLE Orders (
+	order_id int NOT NULL AUTO_INCREMENT,
+    cust_first_name  varchar(255) NOT NULL,
+    cust_last_name  varchar(255) NOT NULL,
+    product_id int NOT NULL,
+    order_date date NOT NULL,
+    amount int NOT NULL,
+    status ENUM ('Processing','Completed','Cancelled') NOT NULL,
+    primary key (order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE
+);
